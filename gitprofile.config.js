66e920bae2d26801afd950d8a38d1a86f07ab153
SarahChanX7 || Skizo <81281058+xznsenpai@users.noreply.github.com>
2023-05-19 22:35:05 +0800

    update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -80,7 +80,7 @@ const config = {
   blog: {
     source: '', // medium | dev
     username: '', // to hide blog section, keep it empty
-    limit: 3, // How many posts to display. Max is 10.
+    limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
