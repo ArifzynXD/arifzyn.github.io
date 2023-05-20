ddaed8585be1e1bc15405f793ce61487dad6a712
Zeiyy || Skizo <81281058+xznsenpai@users.noreply.github.com>
2023-05-20 09:42:21 +0800

    update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -142,6 +142,12 @@ const config = {
       '--rounded-btn': '3rem',
     },
   },
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Made with <a 
+      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      target="_blank"
+      rel="noreferrer"
+    >GitProfile</a> and ❤️`,
 };
 
 export default config;
